4e359abc6cedc5095653c65e438d75ad710a7ab7
SHIM-JINSEO <sjs011828@gmail.com>
2025-06-28 17:49:32 +0900

    feat: boilerplate for react-tailwind-tanstack

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-500 text-white p-4'>
+        hello world
+      </div>
+    </>
+  )
+}
+
+export default App
