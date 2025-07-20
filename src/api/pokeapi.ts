ac992d03313725aac29054b6c66389259c409963
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-07-20 21:14:00 +0900

    add comments to base code

--- src/api/pokeapi.ts
+++ src/api/pokeapi.ts
@@ -1,6 +1,9 @@
 import useSWR from "swr";
+//swr폴더에서 useSWR을 가져와 이 파일에서 사용하게끔 불러오기(import)
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
+//api호출 함수. fetch(url) 서버에 HTTP요청 보내기. .then(res)=>res.json() 응답을 받아서 JSON형태로 변환.
+//url: string(문자열). 만약 age:number(숫자) 이렇게 타입을 지정
 
 export function usePokeApi() {
   const { data, error, isLoading } = useSWR(
@@ -9,11 +12,13 @@ export function usePokeApi() {
   );
   return {
     data: data,
-    isLoading,
+    isLoading: isLoading,
     isError: error,
   };
 }
-
+//funtion usePokeApi.. usePokeApi라는 함수를 만들고 export.. 다른 파일에서 이 함수를 사용할 수 있게 내보내기
+//useSWR(Api주소(url), 데이터 가져오는 방법(fetcher)).. fetcher을 이용해 데이터를 가져오고, 결과를 data, error, isLoading으로 나눠서 받기.
+//return{ isError: error}.. error를 받으면 isError로 저장. 
 export function usePokemonDetails(name: string) {
   const { data, error, isLoading } = useSWR(
     `https://pokeapi.co/api/v2/pokemon/${name}`,
@@ -25,3 +30,6 @@ export function usePokemonDetails(name: string) {
     isError: error,
   };
 }
+//name이라는 문자열을 받는 함수 usePokemonDetaios를 만들고 내보내기
+//${name}은 문자열 안에 name을 넣기. 예를들어 name이 suyeon이면 https://~/suyeon 이렇게 됨.
+//10번줄에서는 "url"쓰고 24번줄에서는 `url`쓰는 이유: 10번은 상수, 24번은 변수
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,26 @@
 import { usePokemonDetails } from "../api/pokeapi";
+//api폴더의 pokeapi.ts파일에서 usePokemonDetails 함수를 가져와 이 파일에서 사용하게끔 불러오기(import)
+
 export default function PokeCard({ name }: { name: string }) {
   const { data: pokemon, isLoading, isError } = usePokemonDetails(name);
+//문자열로 된 name 정보를 PokeCard에 전달(ex: bulbasaur)
+//bulbasaur의 정보를 가져오는 함수 usePokemonDetails를 호출하고, 그 결과를 pokemon으로 저장.
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading data</div>;
+  //isLoading이 true면 Loading... 표시
+ 
+  console.log(`=== ${name} 포켓몬 데이터 ===`);
+  console.log("전체 데이터:", pokemon);
+  console.log("이름:", pokemon.name);
+  console.log("이미지:", pokemon.sprites.front_default);
+  console.log("타입:", pokemon.types);
+  console.log("키:", pokemon.height);
+  console.log("몸무게:", pokemon.weight);
+  console.log("능력치:", pokemon.stats);
+  console.log("능력:", pokemon.abilities);
+  console.log("================================");
+  //console(개발자도구F12)에 해당 정보를 출력(개발자가 데이터를 확인하기 위해)
+
   return (
     <div className="border w-[100px] h-[140px] m-[8px] p-[4px]">
       <div className="w-[100px] h-[100px]">
@@ -11,4 +29,5 @@ export default function PokeCard({ name }: { name: string }) {
       <h3>{name}</h3>
     </div>
   );
+  //return.. 화면에 표시할 부분
 }
