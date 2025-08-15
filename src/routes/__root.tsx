0362473a9ffb314b75b95ca0ed972e305d727029
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-08-15 17:00:31 +0900

    fix: resolve x-overflow issue

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,14 +4,13 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex justify-between absolute w-screen">
-        <Link to="/" className="[&.active]:font-semibold text-[20px] text-[#2a2a2a]">
+      <div className="flex justify-between absolute w-full">
+        <Link to="/" className="[&.active]:font-semibold text-[19px] pl-[10px] pt-1 text-[#2a2a2a]">
           Home
         </Link>{" "}
         <Link to="/about" className="[&.active]:font-bold">
-          <div className="w-auto h-auto gap-[5px] flex items-center">
-            <span className="font-semibold text-[20px] text-[#878787]">Sign In</span>
-            <img src="/loginIcon.svg" alt="login icon" className="w-[28px] h-[28px]"/>
+          <div className="h-autogap-[5px] flex items-center">
+            <span className="font-semibold pt-1 text-[19px] pr-[10px] text-[#2a2a2a]">Sign In </span>
           </div>
         </Link>
       </div>
@@ -19,7 +18,7 @@ export const Route = createRootRoute({
       <hr className="border-[#e8e8e8]"/>
       
 
-      <div className="flex items-center justify-center pt-[40px]">
+      <div className="flex items-center justify-center pt-[35px]">
         <Outlet />
       </div>
 
@@ -31,3 +30,4 @@ export const Route = createRootRoute({
 //[&.active]: ..클래스 활성화
 //<hr />..가로 구분선(className붙여서 스타일링 가능)
 //<Outlet />..자식 컴포넌트 들어갈 자리
+//7번째줄 w-screen 쓰면 우측 스크롤바를 포함한 전체 너비라 ui가 넘침. -> w-full사용
